aa7b4e5cee217c7498a9c9e31d815da812e36c4e
gabs1m <potterhead170025@gmail.com>
2022-09-20 08:39:24 -0300

    separando html em componentes

--- index.php
+++ index.php
@@ -1,47 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>KGSports</title>
-</head>
+<?php require 'componentes/head.php'; ?>
 <body>
-    <header>
-        <div class="header-head">
-            <h1>Logo</h1>
-            <div class="header-searchbar">
-                <form action="busca.php" method="get">
-                    <input type="search" name="busca" class="input-busca" placeholder="Busque um produto...">
-                    <button class="button-busca">
-                        <ion-icon name="search-outline"></ion-icon>
-                    </button>
-                </form>
-            </div>
-            <div class="header-icons">
-                <a href="" class="icon-header"><ion-icon name="heart-outline"></ion-icon> Lista de Desejos</a>
-                <a href="" class="icon-header"><ion-icon name="person-outline"></ion-icon> Login</a>
-                <a href="" class="icon-header"><ion-icon name="cart-outline"></ion-icon> Carrinho</a>
-            </div>
-        </div>
-    </header>
-    <nav class="navbar">
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href="">Link</a>
-    </nav>
+    <?php
+        require 'componentes/header.php';
+        require 'componentes/navbar.php';
+    ?>
     <section id="teste">
         
     </section>
-    <footer>
-        <h2>&copy; Todos os direitos reservados aos respectivos donos</h2>
-    </footer>
+    <?php require 'componentes/footer.php'; ?>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
