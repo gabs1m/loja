47e06af3934084a129e5349561fa30865ec35f74
gabs1m <potterhead170025@gmail.com>
2022-09-19 22:22:39 -0300

    começo do cabeçalho

--- index.php
+++ index.php
@@ -5,17 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>KGSports</title>
 </head>
 <body>
     <header>
-        <h1>Olá, mundo!</h1>
+        <div class="header-head">
+            <h1>Logo</h1>
+            <div class="header-searchbar">
+                <form action="busca.php" method="get">
+                    <input type="search" name="busca" class="input-busca" placeholder="Busque um produto...">
+                    <button class="button-busca">
+                        <ion-icon name="search-outline"></ion-icon>
+                    </button>
+                </form>
+            </div>
+            <div class="header-icons">
+                <a href="" class="icon-header"><ion-icon name="heart-outline"></ion-icon> Lista de Desejos</a>
+                <a href="" class="icon-header"><ion-icon name="person-outline"></ion-icon> Login</a>
+                <a href="" class="icon-header"><ion-icon name="cart-outline"></ion-icon> Carrinho</a>
+            </div>
+        </div>
     </header>
+    <nav class="navbar">
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+        <a href="">Link</a>
+    </nav>
     <section id="teste">
         
     </section>
     <footer>
-        <h2>Olá, mundo!</h2>
+        <h2>&copy; Todos os direitos reservados aos respectivos donos</h2>
     </footer>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
