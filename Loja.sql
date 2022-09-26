9c91e672920b8e3479323bd3e7cd2c4cbb05b2e8
Kant3s <kaykyekauagustavo@gmail.com>
2022-09-26 08:45:36 -0300

    Criação de outraa tabelas

--- Loja.sql
+++ Loja.sql
@@ -14,5 +14,36 @@ CREATE TABLE Loja . Clientes (
 
 CREATE TABLE Loja . Produtos (
     idProduto INT NOT NULL AUTO_INCREMENT,
-    
+    Nome VARCHAR(100) NOT NULL,
+    Descrição VARCHAR(100) NOT NULL,
+    VlrUnitario DECIMAL(9,2) NOT NULL,
+    Lote VARCHAR(15) NOT NULL, 
+    PRIMARY KEY (idProduto)
+)
+
+CREATE TABLE Loja . Kits (
+    idKit INT NOT NULL AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    Descrição VARCHAR(100) NOT NULL,
+    VlrKit DECIMAL(9,2) NOT NULL,
+    PRIMARY KEY (idKit)
+)
+
+CREATE TABLE Loja . Venda (
+    idVenda INT NOT NULL AUTO_INCREMENT,
+    DataVenda DATE NOT NULL,
+    ValorTotal DECIMAL(9,2),
+    idCliente INT NOT NULL,
+    idProduto INT NULL,
+    idKit INT NULL,
+    PRIMARY KEY (idVenda),
+    CONSTRAINT fk_Venda_Clientes
+    FOREIGN KEY (idCliente)
+    REFERENCES Loja . Clientes (idCliente),
+    CONSTRAINT fk_Venda_Produtos
+    FOREIGN KEY (idProduto)
+    REFERENCES Loja . Produtos (idCliente),
+    CONSTRAINT fk_Venda_Kits
+    FOREIGN KEY (idKit)
+    REFERENCES Loja . Kits (idKit),
 )
