7531aeace9844343e87cef4e1a56cc476c6b7c36
gabs1m <potterhead170025@gmail.com>
2022-09-26 09:33:02 -0300

    adição de código da database do kayky

--- database/loja.sql
+++ database/loja.sql
@@ -1,8 +1,7 @@
-DROP DATABASE IF EXISTS loja;
-CREATE DATABASE loja;
+CREATE DATABASE IF NOT EXISTS loja;
 USE loja;
 
-CREATE TABLE loja.clientes (
+CREATE TABLE clientes (
     idCliente INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
@@ -12,7 +11,41 @@ CREATE TABLE loja.clientes (
     PRIMARY KEY (idCliente)
 );
 
-CREATE TABLE loja.produtos (
+CREATE TABLE produtos (
     idProduto INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    descrição VARCHAR(100) NOT NULL,
+    valorUnit DECIMAL(9,2) NOT NULL,
+    lote VARCHAR(15) NOT NULL, 
+    PRIMARY KEY (idProduto)
+)
+
+CREATE TABLE Kits (
+    idKit INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    descrição VARCHAR(100) NOT NULL,
+    valorKit DECIMAL(9,2) NOT NULL,
+    PRIMARY KEY (idKit)
+)
+
+CREATE TABLE venda (
+    idVenda INT NOT NULL AUTO_INCREMENT,
+    dataVenda DATE NOT NULL,
+    valorTotal DECIMAL(9,2),
+    idCliente INT NOT NULL,
+    idProduto INT NULL,
+    idKit INT NULL,
+    PRIMARY KEY (idVenda),
+
+    CONSTRAINT fk_Venda_Clientes
+    FOREIGN KEY (idCliente)
+    REFERENCES loja.clientes (idCliente),
+
+    CONSTRAINT fk_Venda_Produtos
+    FOREIGN KEY (idProduto)
+    REFERENCES loja.produtos (idCliente),
     
+    CONSTRAINT fk_Venda_Kits
+    FOREIGN KEY (idKit)
+    REFERENCES loja.kits (idKit),
 )
