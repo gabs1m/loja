96f38425e5d62f289e9ea6f4b71abce620ad1b5d
gabs1m <potterhead170025@gmail.com>
2022-10-29 12:43:31 -0300

    mudança no hostname da conexao com banco

--- database/conexao.php
+++ database/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-/*$conexao = mysqli_connect('kgsports.epizy.com', 'epiz_32670962', 's9JERCY23Ivi1mh', 'loja');*/
+/*$conexao = mysqli_connect('sql310.epizy.com', 'epiz_32670962', 's9JERCY23Ivi1mh', 'loja');*/
 $conexao = mysqli_connect('localhost', 'root', '', 'loja');
 if(!$conexao){
     die('[ERRO]: '.mysqli_connect_error());
